d9c7e2837d3dbd14597d8afb385b74ae6293225e
kristof39612 <lovacsi.kristof@gmail.com>
2024-10-27 10:33:37 +0100

    OnBackPressed deprecation, closes #23

--- app/src/main/java/hu/bme/aut/szoftverarch/questly/LoginActivity.kt
+++ app/src/main/java/hu/bme/aut/szoftverarch/questly/LoginActivity.kt
@@ -1,6 +1,7 @@
 package hu.bme.aut.szoftverarch.questly
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.commit
 import hu.bme.aut.szoftverarch.questly.fragments.auth.LoginFragment
@@ -16,20 +17,22 @@ class LoginActivity : FragmentActivity() {
                 replace(R.id.fragment_container, LoginFragment())
             }
         }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    supportFragmentManager.popBackStack()
+                } else {
+                    finish()
+                }
+            }
+        })
     }
 
     fun navigateToRegister() {
         supportFragmentManager.commit {
             replace(R.id.fragment_container, RegisterFragment())
             addToBackStack(null)
-        }
-    }
-
-    override fun onBackPressed() {
-        if (supportFragmentManager.backStackEntryCount > 0) {
-            supportFragmentManager.popBackStack()
-        } else {
-            super.onBackPressed()
         }
     }
 }
